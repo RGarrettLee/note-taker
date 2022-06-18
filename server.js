febc400ccaaf475a089908a157bb68cc53d97520
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-17 22:24:58 -0400

    created server.js file with correct routing

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const routes = require('./routes/index');
+const db = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', routes);
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is live at http://localhost:${PORT}`);
+});
